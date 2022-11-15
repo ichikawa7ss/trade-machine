8eda40a7dce7b9c12aae29637e756040c718bbd6
ichikawa7ss <i2s7.chacha@gmail.com>
2022-11-15 23:51:48 +0900

    Add app.

---
+++ trade-machine/trade-machine/trade_machineApp.swift
@@ -0,0 +1,17 @@
+//
+//  trade_machineApp.swift
+//  trade-machine
+//
+//  Created by ichikawa on 2022/11/15.
+//
+
+import SwiftUI
+
+@main
+struct trade_machineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
